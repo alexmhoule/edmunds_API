48794495e2565154283855589e8bcbe485a39da3
alexmhoule <alexmhoule@gmail.com>
2015-09-24 20:21:50 -0500

    models kinda working

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
 $(document).ready(function () {
 
+var makes;
+var models;
+$('.model').hide();
+
 // Get a value for the first drop down
 
-loadAjax();
+loadMake();
+	
+	$('.make').on("change", function(){
+		var makeIndex = $(this).find("option:selected").index();
+		loadModels(makes[makeIndex]);
+		$('.model').show();
+	});
 // Hide second dropdown until first drop down selection made
 
-// Run AJAX
+function loadModels(makes) {
+	console.log(makes);
+	//use line 34 thru 37
+	var option = '';
+	if (makes.models.length>0) {
+		models = makes.models;
+		$.each(models, function (index, value) {
+			option += '<option value="' + index + '">' + value.name + '</option>';
+		});
+		$('.model').append(option);
+	}
+}
 
-function loadAjax() {
+// Run AJAX (Make)
+
+function loadMake() {
 	var data = {
 		part: 'snippet',
+		maxResults: 61,
 		key: 'chw6q5hh9fpbd36wjk9qk3cy'
 	};
 
@@ -17,6 +41,16 @@ function loadAjax() {
 		url: "https://api.edmunds.com/api/vehicle/v2/makes?&view=basic&fmt=json&api_key=chw6q5hh9fpbd36wjk9qk3cy",
 		data: data
 	}).done(function (results) {
+		var option = '';
+		if(results.makes.length>0) {
+			makes = results.makes;
+			$.each(makes, function (index, value) {
+					option += '<option value="' + index + '">' + value.name + '</option>';
+					//option += '<option value="' + results.makes[index].name + '">' + results.makes[index].name + '</option>';
+				});	 
+
+		}
+		$('.make').append(option);
 		console.log(results);
 	}).error(function (error) {
 		console.log(error);
